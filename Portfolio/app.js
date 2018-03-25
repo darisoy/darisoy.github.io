51eba790c6dcb73d60617ef8e97c6074b7824bee
Doruk Arisoy <34922984+d1r1karsy@users.noreply.github.com>
2018-03-24 17:57:39 -0700

    Add files via upload

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,108 +1,61 @@
-/*
-GAME RULES:
+{
+  function renderTime() {
+    var now = new Date();
+    var diem = "am";
+    var h = now.getHours();
+    var min = now.getMinutes();
+    var s = now.getSeconds();
+    var y = now.getFullYear();
+    var mon = now.getMonth() + 1;
+    var d = now.getDate();
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-var scores, roundScore, activePlayer, activeGame;
-
-//var dicePrev;
-init();
-
-document.querySelector(".btn-roll").addEventListener("click", function() {
-  if (activeGame) {
-    //random number for dice
-    var dice1 = Math.floor(Math.random() * 6) + 1;
-    var dice2 = Math.floor(Math.random() * 6) + 1;
-    //display dice
-    var diceDOM1 = document.getElementById("dice-1");
-    diceDOM1.style.display = "block";
-    diceDOM1.src = "dice-" + dice1 + ".png";
-    var diceDOM2 = document.getElementById("dice-2");
-    diceDOM2.style.display = "block";
-    diceDOM2.src = "dice-" + dice2 + ".png";
-    //calculations
-    if (dice1 == 1 || dice2 == 1) {
-      nextPlayer();
-    // } else if (dice == 6 && dicePrev == 6) {
-    //   scores[activePlayer] = 0;
-    //   document.getElementById("score-" + activePlayer).textContent = 0;
-    //   nextPlayer();
-    } else {
-      roundScore += dice1 + dice2;
-      //dicePrev = dice;
-      document.getElementById("current-" + activePlayer).textContent = roundScore;
+    if (h == 0) {
+      h = "00";
+    } else if (h > 12) {
+      h = h - 12;
+      diem = "pm";
+    }
+    if (h < 10) {
+      h = "0" + h;
+    }
+    if (min < 10) {
+      min = "0" + min;
+    }
+    if (s < 10) {
+      s = "0" + s;
+    }
+    if (mon < 10) {
+      mon = "0" + mon;
+    }
+    if (d < 10) {
+      d = "0" + d;
     }
 
+    document.querySelector(".budget__title--month").textContent = "" + mon + "/" + d + "/" + y + " " + h + ":" + min + ":" + s  + diem;
+    setTimeout('renderTime()', 1000);
   }
-});
+  renderTime();
+  intro();
 
-document.querySelector(".btn-hold").addEventListener("click", function() {
-  if (activeGame) {
-    // set winning score
-    var win;
-    var input = document.querySelector(".final-score").value;
-    if (input) {
-      win = input;
-    } else {
-      win = 100;
-    }
-    //adds total score
-    scores[activePlayer] += roundScore;
-    document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
-    //checks for the winner
-    if (scores[activePlayer] >= win) {
-      document.querySelector(".player-" + activePlayer + "-panel").classList.add('winner');
-      document.getElementById("name-" + activePlayer).textContent = "Winner!";
-      document.querySelector(".player-" + activePlayer + "-panel").classList.toggle('active');
-      activeGame = false;
-    } else {
-      nextPlayer();
-    }
+  function intro() {
+    document.querySelector(".links").insertAdjacentHTML('beforeend', '<div id="div1"><li><span><a href="./Portfolio/DorukArisoyResume.pdf" target="_blank">resume</a></span></li><li><span><a href="https://www.linkedin.com/in/dorukarisoy/" target="_blank">linkedin</a></span></li><li><span><a class="projects">projects ></a></span></li><li><span><a href="./Portfolio/DorukArisoyNetworkingPortfolio.pdf" target="_blank">cisco portfolio</a></span></li><li class="popup"><span><a>contact</a></span><span class="popuptext" id="myPopup"></span></li><li></li><li></li></div>');
+    document.querySelector(".logo").style.backgroundImage = "url('Portfolio/logo.jpg')";
+    document.querySelector(".popup").addEventListener("click", function() {
+        console.log("works");
+        var popup = document.getElementById("myPopup");
+        popup.classList.toggle("show");
+    });
+    document.querySelector(".projects").addEventListener("click", function() {
+      var element = document.getElementById("div1");
+      element.remove(element);
+      document.querySelector(".links").insertAdjacentHTML('beforeend','<div id="div1"><li><span><a href="./Budgety/index.html">budgeting app</a></span></li><li><span><a href="./Pig-Game/index.html">pig-game</a></span></li><li><span><a>more projects coming soon...</a></span></li><li><span><a class="back">< back</a></span></li></div>');
+      document.querySelector(".logo").style.backgroundImage = "url('Portfolio/projects.jpg')";
+      document.querySelector(".back").addEventListener("click", function() {
+        var element = document.getElementById("div1");
+        element.remove(element);
+        intro();
+      });
+
+    });
   }
-});
-
-function nextPlayer() {
-  roundScore = 0;
-  //dicePrev = 0;
-  if (activePlayer == 0) {
-    activePlayer = 1;
-  } else {
-    activePlayer = 0;
-  }
-  document.getElementById("current-0").textContent = 0;
-  document.getElementById("current-1").textContent = 0;
-  document.getElementById("dice-1").style.display = "none";
-  document.getElementById("dice-2").style.display = "none";
-  document.querySelector('.player-0-panel').classList.toggle('active');
-  document.querySelector('.player-1-panel').classList.toggle('active');
-}
-
-
-document.querySelector(".btn-new").addEventListener("click", init);
-
-function init() {
-  scores = [0, 0];
-  roundScore = 0;
-  activePlayer = 0;
-  activeGame = true;
-  //dicePrev = 0;
-
-  document.querySelector("#current-0").textContent = 0;
-  document.querySelector("#current-1").textContent = 0;
-  document.querySelector("#score-0").textContent = 0;
-  document.querySelector("#score-1").textContent = 0;
-  document.getElementById("dice-1").style.display = "none";
-  document.getElementById("dice-2").style.display = "none";
-  document.getElementById("name-0").textContent = "Player 1";
-  document.getElementById("name-1").textContent = "Player 2";
-  document.querySelector('.player-0-panel').classList.remove('winner');
-  document.querySelector('.player-1-panel').classList.remove('winner');
-  document.querySelector('.player-1-panel').classList.remove('active');
-  document.querySelector('.player-0-panel').classList.add('active');
-  document.querySelector(".final-score").value = "";
 }
