0097e89cdafbbb95f64727eb2b5b02d92260c2a1
Doruk Arisoy <34922984+d1r1karsy@users.noreply.github.com>
2018-05-31 16:26:12 -0700

    added scrollbar to the new portfolio

--- NewPortfolio/app.js
+++ NewPortfolio/app.js
@@ -1,13 +1,19 @@
 
 function siteSize(){
   var elem = document.getElementById('site');
+  var len = document.getElementById('proj').scrollHeight;
+  var scroll = document.querySelector('.scrollbar');
   if (window.innerWidth > 810) {
     elem.style.width = "750px";
     elem.style.left = ((window.innerWidth / 2) - (750 / 2) - 60) + 'px';
   } else {
     elem.style.left = '0px';
     elem.style.width = (window.innerWidth - 60) + "px";
-
+  }
+  if(window.innerHeight >= len) {
+    scroll.style.height = window.innerHeight + "px";
+  } else {
+    scroll.style.height = (window.innerHeight * window.innerHeight / len) + "px";
   }
 }
 
@@ -18,6 +24,8 @@ function intro(){
   document.getElementById('link4').style.textShadow = "0px 0px 0px black";
   document.getElementById('div1').remove(document.getElementById('div1'));
   document.getElementById('site').insertAdjacentHTML('beforeend', "<div id=\"div1\"><h1><center>Welcome to Doruk Arisoy's Portfolio</center></h1><div><center>Go to the sidebar to view projects</center></div><br><div id=\"container1\"><div id=\"container2\"><p><br></p><h2>Resume</h2><center><object width=\"100%\" height=\"400\" data=\"../Portfolio/DorukArisoyResume.pdf\" id=\"resume\"></object></center><br><a id=\"button\" href=\"../Portfolio/DorukArisoyResume.pdf\" target=\"_blank\">Open in New Page</a> <a id=\"button\" href=\"../Portfolio/DorukArisoyResume.pdf\" download>Download</a><br><br><br></div></div><br><br><br><div id=\"container1\"><div id=\"container2\"><p><br></p><h2>LinkedIn</h2><center><script src=\"//platform.linkedin.com/in.js\" type=\"text/javascript\"></script><script type=\"IN/MemberProfile\" data-id=\"https://www.linkedin.com/in/dorukarisoy\" data-format=\"inline\" data-related=\"false\"></script></center><br><br><br></div></div><br><br><br><div id=\"container1\"><div id=\"container2\"><p><br></p><h2>Networking Portfolio</h2><center><object width=\"100%\" height=\"400\" data=\"../Portfolio/DorukArisoyNetworkingPortfolio.pdf\" id=\"resume\"></object></center><br><a id=\"button\" href=\"../Portfolio/DorukArisoyNetworkingPortfolio.pdf\" target=\"_blank\">Open in New Page</a> <a id=\"button\" href=\"../Portfolio/DorukArisoyNetworkingPortfolio.pdf\" download>Download</a><br><br><br></div></div><br><br><br></div>");
+  document.querySelector(".scrollbar").style.top = "0px";
+  siteSize();
 }
 
 function myReload(){
@@ -29,6 +37,26 @@ siteSize();
 window.addEventListener("resize", siteSize);
 document.addEventListener("touchstart", function(){}, true);
 document.querySelector('.doruka').addEventListener('click', myReload);
+document.getElementById('proj').addEventListener("scroll", function(){
+  var len = document.getElementById('proj');
+  document.querySelector('.scrollbar').style.top = (len.scrollTop / len.scrollHeight) * 100 + "%";
+});
+var mouseID = false;
+document.querySelector(".scrollbar").addEventListener('mousedown', function(){
+  mouseID = true;
+  var len = document.getElementById('proj');
+  initY = event.clientY;
+  document.addEventListener('mousemove', function() {
+    if (mouseID) {
+      len.scrollBy(0, (event.clientY - initY));
+    }
+  });
+  document.addEventListener('mouseup', function() {
+    mouseID = false;
+    delete initY;
+  });
+  document.querySelector('.scrollbar').style.top = (len.scrollTop / len.scrollHeight) * 100 + "%";
+});
 
 document.getElementById('link1').addEventListener('click', function(){
   document.getElementById('link1').style.textShadow = "2px 2px 4px #4586ef";
@@ -37,6 +65,8 @@ document.getElementById('link1').addEventListener('click', function(){
   document.getElementById('link4').style.textShadow = "0px 0px 0px black";
   document.getElementById('div1').remove(document.getElementById('div1'));
   document.getElementById('site').insertAdjacentHTML('beforeend', "<div id=\"div1\"></div>");
+  document.querySelector(".scrollbar").style.top = "0px";
+  siteSize();
 });
 
 document.getElementById('link2').addEventListener('click', function(){
@@ -46,6 +76,8 @@ document.getElementById('link2').addEventListener('click', function(){
   document.getElementById('link4').style.textShadow = "0px 0px 0px black";
   document.getElementById('div1').remove(document.getElementById('div1'));
   document.getElementById('site').insertAdjacentHTML('beforeend', "<div id=\"div1\"></div>");
+  document.querySelector(".scrollbar").style.top = "0px";
+  siteSize();
 });
 
 document.getElementById('link3').addEventListener('click', function(){
@@ -55,6 +87,8 @@ document.getElementById('link3').addEventListener('click', function(){
   document.getElementById('link4').style.textShadow = "0px 0px 0px black";
   document.getElementById('div1').remove(document.getElementById('div1'));
   document.getElementById('site').insertAdjacentHTML('beforeend', "<div id=\"div1\"><div id=\"container1\"><div id=\"container2\"><p><br></p><h1>Clock</h1><p>Description: ..... </p><center><object width=\"100%\" height=\"400\" data=\"../Clock/index.html\" id=\"resume\"></object></center><br><a id=\"button\" href=\"../Clock/index.html\" target=\"_blank\">Open in New Page</a> <a id=\"button\" href=\"http://github.com\" target=\"_blank\">Source Code &lt;/&gt;</a><br><br><br></div></div></div>");
+  document.querySelector(".scrollbar").style.top = "0px";
+  siteSize();
 });
 
 document.getElementById('link4').addEventListener('click', function(){
@@ -64,4 +98,6 @@ document.getElementById('link4').addEventListener('click', function(){
   document.getElementById('link4').style.textShadow = "2px 2px 4px #4586ef";
   document.getElementById('div1').remove(document.getElementById('div1'));
   document.getElementById('site').insertAdjacentHTML('beforeend', "<div id=\"div1\"><div id=\"container1\"><div id=\"container2\"><p><br></p><h1>Old Portfolio</h1><p>Description: ..... </p><center><object width=\"100%\" height=\"400\" data=\"../index.html\" id=\"resume\"></object></center><br><a id=\"button\" href=\"../index.html\" target=\"_blank\">Open in New Page</a> <a id=\"button\" href=\"http://github.com\" target=\"_blank\">Source Code &lt;/&gt;</a><br><br><br></div></div></div>");
+  document.querySelector(".scrollbar").style.top = "0px";
+  siteSize();
 });
